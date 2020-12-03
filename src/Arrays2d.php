a86c85800f25d33fcdf7c6ea15614db426f396ae
xieJialun <36151536+xjryanse@users.noreply.github.com>
2020-12-03 17:36:20 +0800

    无数据设默认值

--- src/Arrays2d.php
+++ src/Arrays2d.php
@@ -53,4 +53,35 @@ class Arrays2d
 
         return $resData;
     }
+    
+    /**
+     * 无数据设默认值
+     * @param array $data           数据
+     * @param string $mainField     主字段：    如日期字段名
+     * @param array $mainColumn     主字段数组：如日期数组
+     * @param string $valueField
+     * @param type $default
+     */
+    public static function noValueSetDefault( array $data, string $mainField, array $mainColumn, string $valueField, $default = "")
+    {
+        //用于存储返回结果的数组
+        $respData = [];
+        //循环全部待取key值
+        foreach( $mainColumn as $v ){
+            //比对数据数组，如有，则赋值
+            foreach( $data as &$value){
+                if($value[ $mainField ] == $v){
+                    $respData[$v] = $value;
+                    break;
+                }
+            }
+            //已赋值的，进入下一个key循环
+            if(isset($respData[$v])){
+                continue;
+            }
+            //未赋值的，设置默认值。主字段$v；值字段$default
+            $respData[$v] = [ $mainField => $v, $valueField => $default ];
+        }
+        return $respData;
+    }
 }
